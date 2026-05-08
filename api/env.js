3afcef323dcb97c9bb98e27fd2f46b14a85150fb
sihyes <eceun@ewha.ac.kr>
2026-05-09 03:30:04 +0900

    Delete api/env.js

--- api/env.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const accessKey = process.env.AWS_ACCESS_KEY_ID || "NOT_FOUND";
-  const secretKey = process.env.AWS_SECRET_ACCESS_KEY || "NOT_FOUND";
-
-  res.status(200).json({
-    AWS_ACCESS_KEY_ID: accessKey,
-    AWS_SECRET_ACCESS_KEY: secretKey,
-  });
-}
