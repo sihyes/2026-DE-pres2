20e64288846964bed38417859cb8847e9dac9a35
sihyes <eceun@ewha.ac.kr>
2026-05-09 03:25:44 +0900

    Update env.js

--- api/env.js
+++ api/env.js
@@ -1,14 +1,9 @@
 export default function handler(req, res) {
-  const accessKey = process.env.AWS_ACCESS_KEY_ID || "";
-  const secretKey = process.env.AWS_SECRET_ACCESS_KEY || "";
+  const accessKey = process.env.AWS_ACCESS_KEY_ID || "NOT_FOUND";
+  const secretKey = process.env.AWS_SECRET_ACCESS_KEY || "NOT_FOUND";
 
   res.status(200).json({
-    AWS_ACCESS_KEY_ID: accessKey
-      ? accessKey
-      : "NOT_FOUND",
-
-    AWS_SECRET_ACCESS_KEY: secretKey
-      ? secretKey
-      : "NOT_FOUND",
+    AWS_ACCESS_KEY_ID: accessKey,
+    AWS_SECRET_ACCESS_KEY: secretKey,
   });
 }
